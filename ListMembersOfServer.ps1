63d18d8e88be476033de0c3b03f277c619cb1fb3
rphil04 <64931748+rphil04@users.noreply.github.com>
2023-03-01 16:36:59 -0600

    Update ListMembersOfServer.ps1

--- ListMembersOfServer.ps1
+++ ListMembersOfServer.ps1
@@ -1,16 +1,22 @@
 Import-Module ActiveDirectory
 
-# Prompt user to enter the server name
-$serverName = Read-Host "Enter the server name"
+# Prompt user to enter the server names
+$servers = Read-Host "Enter the server names separated by commas"
 
-# Get the list of members in the Administrators group
-$adminMembers = Invoke-Command -ComputerName $serverName -ScriptBlock {Get-LocalGroupMember -Group "Administrators"} | Select-Object -ExpandProperty Name
+# Split the server names into an array
+$serverNames = $servers -split ','
 
-# Get the list of members in the Remote Desktop Users group
-$rdpMembers = Invoke-Command -ComputerName $serverName -ScriptBlock {Get-LocalGroupMember -Group "Remote Desktop Users"} | Select-Object -ExpandProperty Name
+foreach ($serverName in $serverNames) {
+    # Get the list of members in the Administrators group
+    $adminMembers = Invoke-Command -ComputerName $serverName -ScriptBlock {Get-LocalGroupMember -Group "Administrators"} | Select-Object -ExpandProperty Name
 
-# Output the lists of members
-Write-Host "Administrators group members:"
-Write-Host $adminMembers
-Write-Host "Remote Desktop Users group members:"
-Write-Host $rdpMembers
+    # Get the list of members in the Remote Desktop Users group
+    $rdpMembers = Invoke-Command -ComputerName $serverName -ScriptBlock {Get-LocalGroupMember -Group "Remote Desktop Users"} | Select-Object -ExpandProperty Name
+
+    # Output the lists of members
+    Write-Host "Server: $serverName"
+    Write-Host "Administrators group members:"
+    Write-Host $adminMembers
+    Write-Host "Remote Desktop Users group members:"
+    Write-Host $rdpMembers
+}
