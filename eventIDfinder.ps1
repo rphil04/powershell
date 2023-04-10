b6e45a8f58f0a79452031bf45d8a5d702d4d0b34
rphil04 <64931748+rphil04@users.noreply.github.com>
2023-04-10 18:14:32 -0500

    Update eventIDfinder.ps1

--- eventIDfinder.ps1
+++ eventIDfinder.ps1
@@ -1,16 +1,45 @@
-$computerName = "REMOTECOMPUTERNAME"
-$eventID = "SPECIFICEVENTID"
+# Prompt the user to enter the hostname of the remote computer
+$computerName = Read-Host "Enter the name of the remote computer"
 
-$found = $false
+# Prompt the user to enter the event ID to search for
+$eventID = Read-Host "Enter the event ID to search for"
 
-$events = Get-WinEvent -ComputerName $computerName -FilterHashtable @{LogName='Application';ID=$eventID} -MaxEvents 10 | Select-Object -Property TimeCreated,Message
+# Prompt the user to enter the name of the log to search in
+$logName = Read-Host "Enter the name of the log to search in"
 
-foreach ($event in $events) {
-    $found = $true
-    Write-Host "Event ID $eventID found on $computerName at $($event.TimeCreated):"
-    Write-Host $event.Message
+try {
+    # Retrieve events from the specified log on the remote computer
+    $events = Get-WinEvent -ComputerName $computerName -FilterHashtable @{LogName=$logName;ID=$eventID} -MaxEvents 10 -ErrorAction Stop
+    
+    # Set a flag to track whether any events were found
+    $found = $false
+    
+    # Loop through each event returned by Get-WinEvent
+    foreach ($event in $events) {
+        # If at least one event is found, set the flag to true
+        $found = $true
+        
+        # Split the event message into separate lines and trim whitespace
+        $messageLines = $event.Message -split "`r`n"
+        $formattedMessage = $messageLines | ForEach-Object { $_.Trim() }
+        
+        # Create a custom object to hold the event information and output it
+        $output = [pscustomobject] @{
+            ComputerName = $computerName
+            EventID = $eventID
+            LogName = $logName
+            TimeCreated = $event.TimeCreated
+            Message = $formattedMessage
+        }
+        Write-Output $output
+    }
+    
+    # If no events were found, output a message indicating that
+    if (!$found) {
+        Write-Host "Event ID $eventID not found on $computerName in $logName log"
+    }
 }
-
-if (!$found) {
-    Write-Host "Event ID $eventID not found on $computerName"
+catch {
+    # If an error occurs, output an error message
+    Write-Error "Error occurred while retrieving events: $_"
 }
